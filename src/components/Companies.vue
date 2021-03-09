82cc588f102792b394902aa5b136b9f91b5a4249
Fran2304 <lilatiravanttiarteaga@gmail.com>
2021-03-09 17:31:13 -0500

    Configuraciones de firebase

---
+++ src/components/Companies.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <!-- <ul v-for="cc in collectionCompanies" :key="cc.id">
+
+      <li >
+        <h1>{{ cc.nombreEmpresa }}</h1>
+      </li>
+    </ul> -->
+
+  </div>
+</template>
+
+<script>
+import { db } from '../firebase';
+
+console.log(db);
+export default {
+  name: 'Companies',
+  props: {
+    // msg: String,
+  },
+  // data() {
+  //   return {
+  //     collectionCompanies: [],
+  //   };
+  // },
+  // firestore: {
+  //   collectionCompanies: db.collection('clientes'),
+  // },
+
+  async created() {
+    const collectionCompanies = await db.collection('clientes').get();
+    collectionCompanies.forEach((doc) => console.log(doc.id, doc.data()));
+
+    const subCollectionCompanies = await db.collection('respuestas/y0yaFFmKtIRQOizhoEVO/respuestas').get();
+    subCollectionCompanies.forEach((doc) => console.log(doc.id, doc.data()));
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+import { db } from './firebase';
+
+console.log(db);
+
 Vue.config.productionTip = false;
 
 new Vue({
